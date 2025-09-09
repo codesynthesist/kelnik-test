cadff4723fab7b46c241a3458f63681398b1a066
Roman K. <roma1901994@gmail.com>
2025-09-10 02:46:37 +0300

    feat: Added nuxt, libs, assets,  data

---
+++ app/pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="flats">
+    <div class="flats__list">
+      <h1>Квартиры</h1>
+    </div>
+    <div class="flats__filter">
+
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  title: 'Квартиры',
+});
+
+</script>
+
+<style scoped lang="scss">
+  .flats {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    padding: 48px 54px;
+
+    &__list {
+    }
+
+    &__filter {
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+  }
+</style>
